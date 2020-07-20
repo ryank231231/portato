a04a6596439b7157df705877fa96eb94b247e3cd
ryank231231 <ryank231231@gmail.com>
2020-07-20 13:29:01 +0800

    FIRST COMMIT

---
+++ xp3info.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <sys/stat.h>
+
+#define FIRST 1
+
+int isfile(char path[])
+{
+    struct _stat buf;
+
+    int result;
+    result = _stat(&path[0], &buf);
+    if (_S_IFREG & buf.st_mode)
+    {
+        return 1;//file
+    }
+    return 0;
+}
+
+int help(void){
+    return 0;
+}
+int main(int argc, char *argv[])
+{
+    char filepath[filepath_enum];
+    int filec_enum = 0;
+
+    if (argc == 1)
+    {
+        help();
+    }
+    else
+    {
+        if (isfile(argv[FIRST]))
+        {
+            printf("it's a file\n");
+        }
+    }
+}
