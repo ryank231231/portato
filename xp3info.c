4c83069a780bcff786244e485bbd12ef2506bc82
ryank231231 <ryank231231@gmail.com>
2020-07-22 00:05:23 +0800

    started xp3 infoing

--- xp3info.c
+++ xp3info.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <sys/stat.h>
 
 #define FIRST 1
-
+#define XP3_TAG1 0x656C6946
 int isfile(char path[]) {
   struct stat buf;
 
@@ -21,15 +22,31 @@ int help(void) {
 
 int getxp3info(char *filepath) {
   FILE *xp3file = NULL;
+  int32_t *bf32=malloc(sizeof(int32_t));
+  int64_t bf64;
   printf("filename:%s\n", filepath);
   xp3file = fopen(filepath, "rb");
   if (NULL == xp3file) {
     printf("cannot open file!\n");
     return 1;
   }
+  
+  fseek(xp3file,0,SEEK_SET);
+  printf("seek it\n");
+  printf("%d\n",sizeof(int32_t));
+  fread(bf32,sizeof(int32_t),1,xp3file);
+  printf("read it\n");
+  printf("0x%x\n",*bf32);
+  if(*bf32==XP3_TAG1){
+      return 0;
+  }else{
+      printf("not a valid xp3 file!\n");
+      return 1;
+  }
   fclose(xp3file);
   return 0;
 }
+
 int main(int argc, char *argv[]) {
   if (argc == 1) {
     help();
