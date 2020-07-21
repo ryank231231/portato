12a88f837619ca250e75379af7a2efcea5383652
ryank231231 <ryank231231@gmail.com>
2020-07-21 23:20:56 +0800

    fixed read file

--- xp3info.c
+++ xp3info.c
@@ -3,33 +3,39 @@
 
 #define FIRST 1
 
-int isfile(char path[])
-{
-    struct _stat buf;
+int isfile(char path[]) {
+  struct stat buf;
 
-    int result;
-    result = _stat(&path[0], &buf);
-    if (_S_IFREG & buf.st_mode)
-    {
-        return 1;//file
-    }
-    return 0;
+  int result;
+  result = stat(&path[0], &buf);
+  if (S_IFREG & buf.st_mode) {
+    return 1; // file
+  }
+  return 0;
 }
 
-int help(void){
-    return 0;
+int help(void) {
+  printf("Usage: xp3info <filename>\n");
+  return 0;
 }
-int main(int argc, char *argv[])
-{
-    if (argc == 1)
-    {
-        help();
+
+int getxp3info(char *filepath) {
+  FILE *xp3file = NULL;
+  printf("filename:%s\n", filepath);
+  xp3file = fopen(filepath, "rb");
+  if (NULL == xp3file) {
+    printf("cannot open file!\n");
+    return 1;
+  }
+  fclose(xp3file);
+  return 0;
+}
+int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    help();
+  } else {
+    if (isfile(argv[FIRST])) {
+      getxp3info(argv[FIRST]);
     }
-    else
-    {
-        if (isfile(argv[FIRST]))
-        {
-            printf("it's a file\n");
-        }
-    }
+  }
 }
