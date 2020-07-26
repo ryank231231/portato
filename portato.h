f4449a59b67c9891e5bc34bffb8a14252bea4276
ryank231231 <ryank231231@gmail.com>
2020-07-26 16:10:36 +0800

    working xp3 head detect

--- portato.h
+++ portato.h
@@ -5,17 +5,21 @@
 
 typedef unsigned char byte_t;
 
-struct KRKR_XP3Header {
+struct KRKR2_XP3Header {
   int32_t Magic;
-  byte_t m2[0x7];
+  byte_t m2[0x7];//placeholder,but necessary.
   int64_t offset;
 };
 
-struct sXP3info {
-  byte_t zlib;
-  int64_t psize;
-  int64_t rsize;
-  byte_t *fileinfo;
+struct KRKR2_XP3_DATA_HEADER {
+  byte_t bZlib;
+  int64_t ArchiveSize;
+  int64_t OriginalSize;
+};
+
+struct KRKR2_XP3_INDEX {
+  struct KRKR2_XP3_DATA_HEADER DataHeader;
+  byte_t Index[1];
 };
 
 int32_t i32conv(int32_t i32) {
@@ -28,4 +32,14 @@ int64_t i64conv(int64_t i64) {
       (((i64 & 0xffff000000000000) >> 16) + ((i64 & 0x0000ffffffff0000) >> 16) +
        ((i64 & 0x000000000000ffff) << 48));
   return i64c;
+}
+
+int32_t i64lowpart(int64_t i64){
+  int32_t i32c =(i64&0x00000000ffffffff);
+  return i32c;
+}
+
+int32_t i64highpart(int64_t i64){
+  int32_t i32c =(i64&0xffffffff00000000);
+  return i32c;
 }
--- xp3info.c
+++ xp3info.c
@@ -21,28 +21,53 @@ int help(void) {
   return EXIT_SUCCESS;
 }
 
+void gowrong(char *reason) {
+  printf("%s\n", reason);
+  exit(EXIT_FAILURE);
+}
+
 int getxp3info(char *filepath) {
   FILE *xp3file = NULL;
-  struct KRKR_XP3Header XP3Header;
+  struct KRKR2_XP3Header XP3Header;
+  struct KRKR2_XP3_DATA_HEADER XP3DataHeader;
+  struct KRKR2_XP3_INDEX XP3Index;
   const int32_t magic = 0x50580d33;  // XP3 magic "XP3"
 
   xp3file = fopen(filepath, "rb");  // open XP3 file
 
   if (NULL == xp3file) {
-    printf("cannot open file!\n");
-    return EXIT_FAILURE;
-  }
+    gowrong("Cannot read file!");
+  }  // exit when file_fail
 
   fseek(xp3file, 0, SEEK_SET);
   fread(&XP3Header.Magic, sizeof(XP3Header.Magic), 1, xp3file);
   if (i32conv(XP3Header.Magic) != magic) {
-    printf("not a valid XP3 archive!\n");
-    exit(EXIT_FAILURE);
+    gowrong("not a valid XP3 archive!");
   }
-  fseek(xp3file, 0, SEEK_SET);
+  printf("Found a XP3 archive\n");
+  XP3DataHeader.OriginalSize = XP3Header.offset;
+  {
+    int64_t offset;
 
+    offset = XP3DataHeader.OriginalSize + SEEK_SET;
+    fseek(xp3file, offset, SEEK_SET);
+    fread(&XP3DataHeader, sizeof(XP3DataHeader), 1, xp3file);
+    printf("\n%x\t%x\n", i64highpart(XP3DataHeader.ArchiveSize),
+           i64lowpart(XP3DataHeader.ArchiveSize));
+    do {
+      if ((i64highpart(XP3DataHeader.ArchiveSize) != 0) ||
+          (i64lowpart(XP3DataHeader.ArchiveSize) == 0)) {
+        printf("a!\n%x\t%x\n", (i64highpart(XP3DataHeader.ArchiveSize) != 0),
+               (i64lowpart(XP3DataHeader.ArchiveSize) == 0));
+        continue;
+      }
+    } while (XP3DataHeader.bZlib & 0x80);
+
+    printf("Orig size:%lld\nComped size:%lld\nComp rate:%d%%\n",
+           XP3DataHeader.OriginalSize, XP3DataHeader.ArchiveSize,
+           (int)(XP3DataHeader.ArchiveSize / XP3DataHeader.OriginalSize));
+  }
   fclose(xp3file);
-
   return EXIT_SUCCESS;
 }
 
